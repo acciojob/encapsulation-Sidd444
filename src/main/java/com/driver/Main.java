b145d3ba70a397668e459633bc2674cd6fb4ae08
Sidd444 <siddharthabharaliassam@gmail.com>
2023-05-15 19:59:00 +0530

    encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        obj.setName(1);
+        System.out.print(obj.getName()); //Error:'Name' has private access in 'com.driver.RWOnly'
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class RWOnly {
-    
+    private int name;
+    public int getName(){
+        return name;
+    }
+    public void setName(int newName){
+         this.name=newName;
+    }
 }
